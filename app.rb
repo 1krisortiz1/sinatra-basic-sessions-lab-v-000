db05421ed432ba8d3dbef844d2075cd0803e9a09
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-08 20:29:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require_relative 'config/environment'
 
+configure do
+    enable :sessions
+    set :session_secret; "secret"
+end
+
 class App < Sinatra::Base
 
-    config do
-        enable :sessions
-        set :session_secret; "secret"
-    end
+    
 end
