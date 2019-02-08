4975d6d82c0d951e599d09fb097faae46ebe2035
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-08 21:03:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-    configure do
+    configure do  #this block enables sessions and sets a sessions_secret
         enable :sessions
         set :sessions_secret, "secret"
     end
 
-    get '/' do
+    get '/' do  #controller action that responds to GET '/'.  Renders to index.erb.
         erb :index
     end
 
